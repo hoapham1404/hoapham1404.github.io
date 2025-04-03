429a72f99c34133eb77ed979e20cdc3c9de9edc6
hoapham1404 <phamquanghoa1404@gmail.com>
2025-04-03 15:42:07 +0700

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,14 +2,11 @@ import { Routes } from '@angular/router';
 import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 export const routes: Routes = [
   {
-    path: '',
-    component: MainLayoutComponent,
-    children: [
-      { path: '', component: HomeComponent },
-      { path: 'about', component: AboutComponent },
-    ],
+    path: '**',
+    component: NotFoundComponent,
   },
 ];
