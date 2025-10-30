3bada85cb8b4a95d75afc3facf59bc1834c098c1
hoapq <hoapq@allexceed.co.jp>
2025-10-30 15:48:07 +0700

    reset

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-import type { Config } from "tailwindcss"
-module.exports = {
-  darkMode: ["class"],
-  content: ["./src/**/*.{html,ts}"],
-  prefix: "",
-  theme: {},
-  plugins: [],
-};
